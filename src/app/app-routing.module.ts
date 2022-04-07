cce75b7f2151ef6ff1069eb4ee2cd9f33cbd7a3e
Ismaël Meziani <meziani-i@outlook.fr>
2022-04-07 18:11:15 +0200

    deactivate footer only for certains components of my choice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 const routes: Routes = [
-  {path:'', redirectTo: 'home', pathMatch: 'full'},
-  {path:'home' , component: HomeComponent}
+  {path:'', redirectTo: 'tutorial', pathMatch: 'full'},
+  {path:'home' , component: HomeComponent},
+  {path:'tutorial', component: TutorialComponent},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  // Below my variables for show footer only on certain components :
+  isFooterDisplayed = 'show-footer';
+  // Here my string array for regroup the components who they doesn't needs display footer , juste add component of your choice
+  componentDoesntNeedFooter: string[] = ['TutorialComponent'];
   constructor() {}
+  componentAdded(e){
+    // If you need decomment this line console.log(e.constructor.name);
+    this.isFooterDisplayed = this.componentDoesntNeedFooter.includes(e.constructor.name) ? 'hide-footer' : 'show-footer';
+  }
 }
